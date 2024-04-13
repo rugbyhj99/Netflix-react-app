8555e323518b2adcd415ac62b30f5e29d8dc14e4
Hojae Lee <rugbyhj99@naver.com>
2024-04-13 11:41:18 +0900

    상세페이지 완성?

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -32,8 +32,7 @@ const MovieDetailPage = () => {
   return (
     <div className="detail-page">      
       <div className="detail-content">
-      <div className="detail-background" style={{ backgroundImage: `url(https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie?.backdrop_path})` }}></div>
-        
+        <div className="detail-background" style={{ backgroundImage: `url(https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie?.backdrop_path})` }}></div>
           <Row>
             <Col lg={6} xs={12} className="order-2 order-lg-1">
               <MovieDetailPageInfo movie={movie} creditsData={creditsData} id={id} />
@@ -41,12 +40,13 @@ const MovieDetailPage = () => {
             <Col lg={6} xs={12} className="detail-content-img-area order-1 order-lg-2">
               <img src={`https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie?.poster_path}`} className="detail-img" />
             </Col>            
-          </Row>
-        
+          </Row>        
       </div>
+
       <div className="detail-review">
         <MovieDetailReview review={review}/>
       </div>
+      
       <div>
         <MovieDetailRecommend recommend={recommend}/>
       </div>
